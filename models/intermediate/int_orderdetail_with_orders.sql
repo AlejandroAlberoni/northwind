454cb4b3a69c04d4e33c0a7d37062fc2263956df
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2026-04-08 00:53:27 -0300

    fix: customer name typos in orders

--- models/intermediate/int_orderdetail_with_orders.sql
+++ models/intermediate/int_orderdetail_with_orders.sql
@@ -1,3 +1,22 @@
+with 
+    orders as (
+    select
+        *
+    from {{ ref('stg_order') }}
+    ),
+    corrections as (
+        select 
+            * 
+        from {{ ref ('customer_corrections') }}
+    ),
+    orders_corrected as (
+        select
+            orders.* except(CustomerId),
+            coalesce(c.correct_customer_id, orders.CustomerId) as CustomerId
+        from orders
+        left join corrections c
+            on orders.CustomerId = c.wrong_customer_id
+    )
 select
     od.*,
     o.CustomerId,
@@ -10,4 +29,4 @@ select
     o.ShipRegion,
     o.ShipCountry
 from {{ ref('stg_orderdetail') }} od
-left join {{ ref('stg_order') }} o on od.OrderId = o.Id
+left join orders_corrected o on od.OrderId = o.Id
